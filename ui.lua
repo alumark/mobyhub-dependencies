bebbe985b7555b3b86826cdf478c14ebf62fa340
mobyboy <thekckfactor@gmail.com>
2020-06-15 12:42:48 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -2,4 +2,4 @@ _G.ToggleColor = Color3.fromRGB(1,243,106)
 _G.ButtonColor = Color3.fromRGB(1,243,106)
 _G.SliderColor = Color3.fromRGB(1,243,106)
 
-return loadstring(game:HttpGet(('https://pastebin.com/raw/FsJak6AT')))() -- It's obfuscated, I won't let you see my ugly coding skills. =)
+return loadstring(game:HttpGet(('https://pastebin.com/raw/FsJak6AT')))()
