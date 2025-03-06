8697bb940b00aaad37bb0ac6f29667144e1a4bc6
ciara r k. <thekckfactor@gmail.com>
2025-03-05 19:42:47 -0600

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -2,4 +2,4 @@ _G.ToggleColor = Color3.fromRGB(1,243,106)
 _G.ButtonColor = Color3.fromRGB(1,243,106)
 _G.SliderColor = Color3.fromRGB(1,243,106)
 
-return loadstring(game:HttpGet(('https://gist.githubusercontent.com/mobycs/668fb1170cfd3e4809c1dee070d04c57/raw/1c5be0102686f8a31b1788e1dcc53c9a4a7cce28/lib.lua')))()
+return loadstring(game:HttpGet(('https://gist.githubusercontent.com/ciraph-git/668fb1170cfd3e4809c1dee070d04c57/raw/1c5be0102686f8a31b1788e1dcc53c9a4a7cce28/lib.lua')))()
